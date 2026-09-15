6dfd4fd2fb105cf74d48d2c96146374f3af85f7c
kimi <kimi@local>
2026-09-15 10:48:06 +0800

    Replace toy category with 15 new designer images

--- app/site-data.js
+++ app/site-data.js
@@ -51,10 +51,7 @@ const sunglassesImages = [
 ];
 
 const toyImages = [
-  "toy-01.jpg","toy-02.jpg","toy-03.jpg","toy-04.jpg","toy-05.jpg","toy-06.png","toy-07.png",
-  "toy-08.jpg","toy-09.jpg","toy-10.jpg","toy-11.jpg","toy-12.jpg","toy-13.jpg","toy-14.jpg",
-  "toy-15.jpg","toy-16.jpg","toy-17.jpg","toy-20.jpg","toy-22.jpg","toy-23.jpg","toy-24.jpg",
-  "toy-26.jpg"
+  "toy-01.jpg","toy-02.jpg","toy-03.jpg","toy-04.jpg","toy-05.jpg","toy-06.jpg","toy-07.jpg","toy-08.jpg","toy-09.jpg","toy-10.jpg","toy-11.jpg","toy-12.jpg","toy-13.jpg","toy-14.jpg","toy-15.jpg"
 ];
 
 const homeTextileImages = [
